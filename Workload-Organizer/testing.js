750e95f3242f82ac72a714b3899e5c5382cbb77b
aripnokihs <146025944+aripnokihs@users.noreply.github.com>
2024-03-30 14:11:04 -0400

    Update Workload-Organizer

--- Workload-Organizer/testing.js
+++ Workload-Organizer/testing.js
@@ -1,3 +1,18 @@
 console.log('hello world');
 console.log('12345');
 console.log('67890');
+global.bubble = "Bubbles";
+console.log(global.bubble);
+
+const {EventEmitter} = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('fun', () => {
+    console.log("I am happy :)");
+})
+
+eventEmitter.on('sad', () => {
+    console.log("I am sad.");
+})
+
+event
